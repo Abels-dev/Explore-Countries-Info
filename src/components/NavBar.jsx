1b1ae3a1271114c99ae0d8e999e90a3d0ad85333
abel <abelgetachew143@gmail.com>
2025-06-18 16:10:31 +0300

    implemented the navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,38 @@
+import { useEffect, useState } from "react";
+
 const NavBar = () => {
+   const [currentTheme, setCurrentTheme] = useState("light");
+   const handleClick = () => {
+      document.body.classList.toggle("dark-mode");
+      if (currentTheme === "light") {
+         setCurrentTheme("dark");
+         localStorage.setItem("theme", "dark");
+      } else {
+         setCurrentTheme("light");
+         localStorage.setItem("theme", "light");
+      }
+   };
+
+   useEffect(() => {
+      const preferredTheme = localStorage.getItem("theme") || "light";
+      setCurrentTheme(preferredTheme);
+      document.body.classList.toggle("dark-mode", preferredTheme === "dark");
+   }, []);
    return (
-      <div>
+      <header>
          <h1>Where in the World?</h1>
          <div className="dark-mode-toggle-container">
-            <button className="dark-mode-toggle">
-               <img src="../assets/moon.png" alt="light-mode"/>
+            <button className="dark-mode-toggle" onClick={handleClick}>
+               <img
+                  src={`/assets/${
+                     currentTheme === "light" ? "sun" : "moon"
+                  }.png`}
+                  alt={`${currentTheme}-mode`}
+               />
             </button>
-            <span>Dark Mode</span>
+            <span>{currentTheme === "light" ? "Light Mode" : "Dark Mode"}</span>
          </div>
-      </div>
+      </header>
    );
 };
 
